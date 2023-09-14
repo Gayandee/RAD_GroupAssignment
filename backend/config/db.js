64cbd1894b3ce95f6b8598d1b9982f3413b28a60
Gayandee <2021cs153@stu.ucsc.cmb.ac.lk>
2023-09-14 12:37:02 +0530

    Push DB

---
+++ backend/config/db.js
@@ -0,0 +1,31 @@
+//Connection file to mongo db
+import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect("mongodb://localhost:27017/myPlanner");
+//     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+//   } catch (error) {
+//     console.error(`Error: ${error.message}`.red.bold);
+//     process.exit();
+//   }
+// };
+
+// export default connectDB;
+
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.bold);
+    process.exit();
+  }
+};
+
+export default connectDB;
